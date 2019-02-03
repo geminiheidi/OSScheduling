9c7ac99c2550dc3efe308d4771ffd54ea7fceb35
Jing Xu <jingxu526@gmail.com>
2019-02-03 09:57:31 -0800

    set getter setter compareTo in process

--- src/Process.java
+++ src/Process.java
@@ -1,2 +1,41 @@
-public class Process {
+public class Process implements Comparable<Process>{
+    private float arrivalTime
+    private int startQuanta;
+    private float serviceTime;
+    private int finishQuanta;
+    private int priority;
+    private char name;
+
+    public float getArrivalTime(){
+        return arrivalTime;
+    }
+    public int getStartQuanta(){
+        return startQuanta;
+    }
+    public float getServiceTime(){
+        return serviceTime;
+    }
+    public int getFinishQuanta(){
+        return finishQuanta;
+    }
+    public int getPriority(){
+        return priority;
+    }
+    public char getName(){
+        return name;
+    }
+
+    public void setStartQuanta(int newStartQuanta){
+        startQuanta = newStartQuanta;
+    }
+    public void setServiceTime(float newServeiceTime){
+        serviceTime = newServeiceTime;
+    }
+
+
+    @Override
+    public int compareTo(Process other){
+        return Float.compare(arrivalTime, other.arrivalTime);
+
+    }
 }
